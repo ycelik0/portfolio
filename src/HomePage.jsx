34864010792a65b5f9c5496b8b72aabea1f20907
Yusuf <yc5693410@gmail.com>
2022-11-30 12:58:32 +0100

    navbar beetje verder begin aan main

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function HomePage() {
   return (
     <main>
-      
+      <section className="home__hero-section bg-primary text-white">
+        <h1 className="text-[2.75rem] py-5 text-center">Frontend Web Developer</h1>
+        <p className="text-gray-500 py-5">asd</p>
+      </section>
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,75 +3,54 @@ import { Link } from "react-router-dom";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
 const Navbar = () => {
-  const [navbarOn, setNavbarOn] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(false);
-
-  const showNavbar = () => {
-    setNavbarOn(true);
+  const [navbarStatus, setNavbarStatus] = useState("navbar-collapse");
+  console.log(navbarStatus);
+  const navbarHandler = () => {
+    setNavbarStatus("navbar-collapsed");
+    navbarStatus === "navbar-collapsed"
+      ? setNavbarStatus("navbar-collapse")
+      : setNavbarStatus("navbar-collapsed");
   };
-
-  const responsiveNavHandler = () => {
-    if (window.innerWidth < 1200) {
-      setWindowWidth(true);
-    } else {
-      setWindowWidth(false)
-    }
-  };
-
-  useEffect(() => {
-    responsiveNavHandler()
-  }, [window.innerWidth])
-  
-
-  window.addEventListener("resize", responsiveNavHandler);
 
   return (
     <div className="navbars">
       <header
-        className={"bg-primary flex flex-col text-light p-5 w-full h-full"}
-        style={{
-          display: windowWidth ? "block" : "none",
-        }}
-      >
-        <button className="w-fit self-end align-middle" onClick={showNavbar}>
+        className={"bg-primary flex flex-col text-light align-middle w-full h-[64px]"}>
+        <button
+          className="w-fit self-end align-middle z-50 p-5"
+          onClick={navbarHandler}
+        >
           <MenuRoundedIcon />
         </button>
         <nav
-          className="navbar-collapse bg-primary flex flex-col text-light p-5 w-full h-full absolute z-[10] place-self-center"
-          style={{
-            left: !navbarOn ? "-100%" : "0",
-          }}
+          className={`${navbarStatus} bg-primary flex flex-col text-light w-full h-full z-[10] absolute justify-center`}
         >
           <ul className="flex flex-col justify-center text-center">
-            <li className="py-1">
-              <Link to={"/"} className="text-[2rem] tracking-widest">
+            <li className="py-2">
+              <Link to={"/"} className="text-[2rem] tracking-widest" onClick={navbarHandler}>
                 HOME
               </Link>
             </li>
-            <li className="py-1">
-              <Link to={"/"} className="text-[2rem] tracking-widest">
+            <li className="py-2">
+              <Link to={"/"} className="text-[2rem] tracking-widest" onClick={navbarHandler}>
                 ABOUT
               </Link>
             </li>
-            <li className="py-1">
-              <Link to={"/"} className="text-[2rem] tracking-widest">
+            <li className="py-2">
+              <Link to={"/"} className="text-[2rem] tracking-widest" onClick={navbarHandler}>
                 PROJECT
               </Link>
             </li>
-            <li className="py-1">
-              <Link to={"/"} className="text-[2rem] tracking-widest">
+            <li className="py-2">
+              <Link to={"/"} className="text-[2rem] tracking-widest" onClick={navbarHandler}>
                 CONTACT
               </Link>
             </li>
           </ul>
         </nav>
       </header>
-      <aside
-        className="side-navbar max-w-[350px] h-full bg-primary text-light flex flex-col justify-between align-middle py-[10rem]"
-        style={{
-          display: windowWidth ? "none" : "block",
-        }}
-      >
+      {/* <aside
+        className="side-navbar max-w-[350px] h-full bg-primary text-light flex flex-col justify-between align-middle py-[10rem]">
         <div className="logo w-[100px] h-[100px] bg-blue-500 mb-[40%] mx-auto"></div>
         <nav className="w-full">
           <ul className="flex flex-col justify-center text-center">
@@ -98,7 +77,7 @@ const Navbar = () => {
           </ul>
         </nav>
         <div className="div my-auto">links</div>
-      </aside>
+      </aside> */}
     </div>
   );
 };
