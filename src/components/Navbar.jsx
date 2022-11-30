17d7912f78c4c87240c814a53f61f4bfa32122e6
Yusuf Celik <yc5693410@gmail.com>
2022-11-30 10:31:14 +0100

    navbar beetje responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,106 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
-function Navbar() {
+const Navbar = () => {
+  const [navbarOn, setNavbarOn] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(false);
+
+  const showNavbar = () => {
+    setNavbarOn(true);
+  };
+
+  const responsiveNavHandler = () => {
+    if (window.innerWidth < 1200) {
+      setWindowWidth(true);
+    } else {
+      setWindowWidth(false)
+    }
+  };
+
+  useEffect(() => {
+    responsiveNavHandler()
+  }, [window.innerWidth])
+  
+
+  window.addEventListener("resize", responsiveNavHandler);
+
   return (
-    <aside className="max-w-[350px] h-screen bg-primary text-light flex flex-col align-middle">
-      <div className="logo w-[100px] h-[100px] bg-blue-500  my-[50%]"></div>
-      <nav className="w-full">
-        <ul className="flex flex-col justify-center text-center">
-          <li>
-            <Link to={'/'}>
-              <p className="text-3xl">
-                Home
-              </p>
-            </Link>
-          </li>
-          <li>
-            <Link to={'/'}>
-              <p className="text-3xl">
-                About
-              </p>
-            </Link>
-          </li>
-          <li>
-            <Link to={'/'}>
-              <p className="text-3xl">
-                Project
-              </p>
-            </Link>
-          </li>
-          <li>
-            <Link to={'/'}>
-              <p className="text-3xl">
-                Contact
-              </p>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </aside>
-  )
-}
+    <div className="navbars">
+      <header
+        className={"bg-primary flex flex-col text-light p-5 w-full h-full"}
+        style={{
+          display: windowWidth ? "block" : "none",
+        }}
+      >
+        <button className="w-fit self-end align-middle" onClick={showNavbar}>
+          <MenuRoundedIcon />
+        </button>
+        <nav
+          className="navbar-collapse bg-primary flex flex-col text-light p-5 w-full h-full absolute z-[10] place-self-center"
+          style={{
+            left: !navbarOn ? "-100%" : "0",
+          }}
+        >
+          <ul className="flex flex-col justify-center text-center">
+            <li className="py-1">
+              <Link to={"/"} className="text-[2rem] tracking-widest">
+                HOME
+              </Link>
+            </li>
+            <li className="py-1">
+              <Link to={"/"} className="text-[2rem] tracking-widest">
+                ABOUT
+              </Link>
+            </li>
+            <li className="py-1">
+              <Link to={"/"} className="text-[2rem] tracking-widest">
+                PROJECT
+              </Link>
+            </li>
+            <li className="py-1">
+              <Link to={"/"} className="text-[2rem] tracking-widest">
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <aside
+        className="side-navbar max-w-[350px] h-full bg-primary text-light flex flex-col justify-between align-middle py-[10rem]"
+        style={{
+          display: windowWidth ? "none" : "block",
+        }}
+      >
+        <div className="logo w-[100px] h-[100px] bg-blue-500 mb-[40%] mx-auto"></div>
+        <nav className="w-full">
+          <ul className="flex flex-col justify-center text-center">
+            <li className="py-2">
+              <Link to={"/"} className="text-[1.25rem] tracking-widest">
+                HOME
+              </Link>
+            </li>
+            <li className="py-2">
+              <Link to={"/"} className="text-[1.25rem] tracking-widest">
+                ABOUT
+              </Link>
+            </li>
+            <li className="py-2">
+              <Link to={"/"} className="text-[1.25rem] tracking-widest">
+                PROJECT
+              </Link>
+            </li>
+            <li className="py-2">
+              <Link to={"/"} className="text-[1.25rem] tracking-widest">
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="div my-auto">links</div>
+      </aside>
+    </div>
+  );
+};
 
 export default Navbar;
